c1a9c83424b200e8627702cb8ec8e3955732f423
vmullerdesigner <75128264+vmullerdesigner@users.noreply.github.com>
2025-07-03 21:30:51 +0200

    castevinia stairs baby!

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -3,11 +3,13 @@ extends CharacterBody2D
 @export var horizontal_acceleration := 100
 @export var horizontal_deceleration := 800
 @export var max_horizontal_speed := 40
-@export var jump_velocity := -50
+@export var jump_velocity := -40
 @export var gravity_up := 150
 @export var gravity_down := 300
 
-
+func _ready() -> void:
+	set_collision_mask_value(4, false)
+	
 func _physics_process(delta: float) -> void:
 	# Gravity
 	if not is_on_floor():
@@ -15,6 +17,7 @@ func _physics_process(delta: float) -> void:
 			velocity.y += gravity_up * delta
 		else:
 			velocity.y += gravity_down * delta
+	
 	# Horizontal Movement
 	var x_input = Input.get_axis("left", "right")
 	
@@ -28,11 +31,18 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 	
-	# Down the one-way
-	if Input.is_action_just_pressed("down") and is_on_floor():
-		print('down')
+	# Down the staircase
+	if Input.is_action_just_pressed("down"):
+		set_collision_mask_value(5, false)
 		
-		velocity.y = 1
-
-
+	if Input.is_action_just_released("down"):
+		set_collision_mask_value(5, true)
+	
+	#Climb stairs
+	if Input.is_action_just_pressed("up"):
+		set_collision_mask_value(4, true)
+	
+	if Input.is_action_just_released("up"):
+		set_collision_mask_value(4, false)
+		
 	move_and_slide()
