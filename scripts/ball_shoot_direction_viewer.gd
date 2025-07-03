d4c2d9ccb85105bee4437220a71e37283e143eeb
Stéphane Loegel <loegel.stephane@gmail.com>
2025-07-03 22:38:40 +0200

    Ball: add a very quick PoC to test ball shoot

---
+++ scripts/ball_shoot_direction_viewer.gd
@@ -0,0 +1,15 @@
+extends Line2D
+
+@export var ball: Ball
+@export var forceColor: Gradient
+
+func _process(delta: float) -> void:
+	set_point_position(1, getForceVector())
+	position = ball.position
+	visible = ball.canBeShoot
+	
+	var forcePercentage: float = inverse_lerp(ball.forceMin, ball.forceMax, ball.force)
+	set_default_color(forceColor.sample(forcePercentage))
+
+func getForceVector() -> Vector2:
+	return Vector2(cos(ball.angle), sin(ball.angle)) * ball.force 
