f9efe2ffe778ac2eace3cd1eef4b34ab1e67ede6
Stéphane Loegel <loegel.stephane@gmail.com>
2025-07-09 09:16:06 +0200

    Ball: improve "perf" and improve rotation visibility

--- scripts/ball_shoot_direction_viewer.gd
+++ scripts/ball_shoot_direction_viewer.gd
@@ -4,9 +4,11 @@ extends Line2D
 @export var forceColor: Gradient
 
 func _process(delta: float) -> void:
+	visible = ball.canBeShoot
+	if (!visible): return
+
 	set_point_position(1, getForceVector())
 	position = ball.position
-	visible = ball.canBeShoot
 	
 	var forcePercentage: float = inverse_lerp(ball.forceMin, ball.forceMax, ball.force)
 	set_default_color(forceColor.sample(forcePercentage))
