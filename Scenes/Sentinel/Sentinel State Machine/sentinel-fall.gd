fa34de2d302e0a0a374610aea5a4617e15bd02bf
vmullerdesigner <75128264+vmullerdesigner@users.noreply.github.com>
2025-07-17 08:38:00 +0200

    Added bin

---
+++ Scenes/Sentinel/Sentinel State Machine/sentinel-fall.gd
@@ -0,0 +1,24 @@
+extends State
+
+@export var idle_state: State
+
+@export var landing_threshold: float = 600
+
+
+@export var fall_gravity_multiplier: float = 2
+
+var last_y_velocity: float = 0
+
+func enter() -> void:
+	super()
+
+func process_physics(delta: float) -> State:
+	
+	parent.velocity.y += gravity * fall_gravity_multiplier * delta
+	
+	parent.move_and_slide()
+	
+	if parent.is_on_floor():
+		return idle_state
+	
+	return null
